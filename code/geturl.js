4caa6c557fd8a9d9396459ca02d9f9f8b9580d3d
mehrdad seyfi houjaghan <seyfi.mehrdad@gmail.com>
2021-12-26 07:38:37 +0200

    Update 12/26/2021  7:38:37.17

---
+++ code/geturl.js
@@ -0,0 +1,7 @@
+async function file_get_contents(uri, callback) {
+    let res = await fetch(uri),
+        ret = await res.text(); 
+    return callback ? callback(ret) : ret; // a Promise() actually.
+}
+
+file_get_contents("https://api.webgold.ir/data/?u=instinfofast&i=2400322364771558", console.log);
