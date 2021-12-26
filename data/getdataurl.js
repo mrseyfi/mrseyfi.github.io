ca991be26e5ee49013cfaf9e9f0a9e5a9237b346
mehrdad seyfi houjaghan <45231041+mrseyfi@users.noreply.github.com>
2021-12-26 09:21:21 +0330

    Update getdataurl.js

--- data/getdataurl.js
+++ data/getdataurl.js
@@ -1,7 +1,9 @@
-async function file_get_contents(uri, callback) {
+  <script>
+  async function file_get_contents(uri, callback) {
     let res = await fetch(uri),
         ret = await res.text(); 
     return callback ? callback(ret) : ret; // a Promise() actually.
 }
 
 file_get_contents("https://api.webgold.ir/data/?u=instinfofast&i=2400322364771558", console.log);
+</script>
